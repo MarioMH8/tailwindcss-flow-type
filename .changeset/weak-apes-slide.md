72627136ceb78dd76bcbb3bca4b2682315bbb3e9
MarioMH8 <mhidalgomario@gmail.com>
2025-12-05 00:35:13 +0100

    fix: update default line heights

---
+++ .changeset/weak-apes-slide.md
@@ -0,0 +1,5 @@
+---
+"tailwindcss-flow-type": patch
+---
+
+Update default line heights
--- src/theme/flow-theme.default.ts
+++ src/theme/flow-theme.default.ts
@@ -17,18 +17,18 @@ const FLOW_TYPE_THEME_TEXT_DEFAULT: TailwindFlowTypeTheme = {
 };
 
 const FLOW_TYPE_THEME_LINE_HEIGHT_DEFAULT: TailwindFlowTypeTheme = {
-	'2xl': '1.2',
-	'3xl': '1.2',
-	'4xl': '1.1',
-	'5xl': '1.1',
-	'6xl': '1.1',
-	'7xl': '1',
-	'8xl': '1',
-	'9xl': '1',
+	'2xl': '1.5',
+	'3xl': '1.5',
+	'4xl': '1.5',
+	'5xl': '1.4',
+	'6xl': '1.4',
+	'7xl': '1.4',
+	'8xl': '1.4',
+	'9xl': '1.3',
 	base: '1.6',
 	lg: '1.6',
 	sm: '1.6',
-	xl: '1.2',
+	xl: '1.5',
 	xs: '1.6',
 };
 
