6c11679be9ee5d3ecaace157f4f8f45913f2a622
MarioMH8 <mhidalgomario@gmail.com>
2026-08-03 11:26:46 +0200

    ci(pages): deploy demo on stable releases

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -1,4 +1,5 @@
 import path from 'node:path';
+import process from 'node:process';
 import { fileURLToPath } from 'node:url';
 
 import { defineConfig } from 'vite';
@@ -6,6 +7,7 @@ import { defineConfig } from 'vite';
 const directory = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
+	base: process.env['VITE_BASE_PATH'] ?? '/',
 	build: {
 		emptyOutDir: true,
 		outDir: path.resolve(directory, 'dist'),
